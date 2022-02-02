b19295ac5904614a8c394f5153a4e49a6093032a
kochenma <kochen.michael@gmail.com>
2022-02-01 16:52:27 -0800

    Distro update.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='SBbadger',  # How you named your package folder (MyLib)
     packages=['SBbadger'],  # Chose the same as "name"
-    version='0.1.10.1',
+    version='0.1.11',
     license='Apache',
     description='Synthetic biochemical reaction networks with definable degree distributions.',  # Give a short description about your library
     author='YOUR NAME',  # Type in your name
     author_email='kochenma@uw.edu',  # Type in your E-Mail
     url='https://github.com/sys-bio/SBbadger',  # Provide either the link to your github or to your website
-    download_url='https://github.com/sys-bio/SBbadger/archive/refs/tags/v0.1.10.1-alpha.tar.gz',  # I explain this later on
+    download_url='https://github.com/sys-bio/SBbadger/archive/refs/tags/v0.1.11-alpha.tar.gz',  # I explain this later on
     keywords=['Systems biology', 'Benchmark Models'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'numpy',
