d1991e7737d291a9fa7cf2a7ce4231379ee45170
kochenma <kochen.michael@gmail.com>
2022-02-03 18:29:47 -0800

    Distro update.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'pydot',
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Alpha',
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
