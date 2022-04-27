26cb72f1a53570194fa7df732dceb4bfa22a2183
kochenma <kochen.michael@gmail.com>
2022-04-27 00:00:19 -0700

    Fixed the absence of a version method.

--- SBbadger/__init__.py
+++ SBbadger/__init__.py
@@ -2,4 +2,4 @@ import SBbadger
 from SBbadger import buildNetworks
 from SBbadger import generate
 from SBbadger import generate_serial
-#from SBbadger.generate import models
+from SBbadger._version import __version__
--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 from setuptools import setup
+import os.path
+import codecs
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# The following two methods were copied from
+# https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            print (line)
+            delim = '"' if '"' in line else "'"
+            print ('delim = ', delim)
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 setup(
     name='SBbadger',
     packages=['SBbadger'],
-    version='1.0.2',
+    version=get_version("SBbadger/_version.py"),
     license='Apache',
     description='Synthetic biochemical reaction networks with definable degree distributions.',
     long_description=long_description,
