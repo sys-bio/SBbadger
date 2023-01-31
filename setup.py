7e485ea38c47c75b19021d6971442d601f2f2094
mialko <kochen.michael@gmail.com>
2023-01-30 18:39:30 -0800

    Updated version number.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     author='Michael Kochen',
     author_email='kochenma@uw.edu',
     url='https://github.com/sys-bio/SBbadger',
-    download_url='https://github.com/sys-bio/SBbadger/archive/refs/tags/v1.2.6.tar.gz',
+    download_url='https://github.com/sys-bio/SBbadger/archive/refs/tags/v1.2.7.tar.gz',
     keywords=['Systems biology', 'Benchmark Models'],
     install_requires=[
         'numpy',
